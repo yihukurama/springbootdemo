a045d441e35a3b0de09e58ba13338be179dc3a84
yihukurama <479757405@qq.com>
2016-08-06 18:27:48 +0800

    配置打成war包进行部署

--- src/main/java/com/yihukurama/springbootdemo/Application.java
+++ src/main/java/com/yihukurama/springbootdemo/Application.java
@@ -1,6 +1,9 @@
 package com.yihukurama.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,10 +12,14 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+    
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
-public class Application {
-    
-    
     
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
