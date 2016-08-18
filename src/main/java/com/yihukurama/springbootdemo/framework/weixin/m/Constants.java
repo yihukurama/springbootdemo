828b40e2adafcb70395016d4cacbd1b55c4686ac
yihukurama <479757405@qq.com>
2016-08-18 16:39:57 +0800

    用微信测试公众帐号进行开发

--- src/main/java/com/yihukurama/springbootdemo/framework/weixin/m/Constants.java
+++ src/main/java/com/yihukurama/springbootdemo/framework/weixin/m/Constants.java
@@ -10,8 +10,12 @@ package com.yihukurama.springbootdemo.framework.weixin.m;
  * @author Dengshuai
  */
 public class Constants {
-    public static final String TOKEN = "yihukurama";
-    public static final String APPID = "wx997dc075a8548c80";
-    public static final String APPSECRET = "db571c752335f76e9dad840fd1d2642e";
+	public static final String TOKEN = "yihukurama";
+    public static final String APPID = "wxeb2343c9f831f559";
+    public static final String APPSECRET = "98946cb68f1e3c88855c053b1d881dbb";
     public static String accessToken = "";
+//    public static final String TOKEN = "yihukurama";
+//    public static final String APPID = "wx997dc075a8548c80";
+//    public static final String APPSECRET = "db571c752335f76e9dad840fd1d2642e";
+//    public static String accessToken = "";
 }
